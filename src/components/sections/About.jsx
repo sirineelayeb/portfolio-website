1cbabd8e3bed9b4eaeb8483f4775f712b8d2ca88
Sirine Elayeb <sirineelayeb21gmail.com>
2026-01-01 15:23:33 +0100

    feat: add CV download functionality to About section

--- src/components/sections/About.jsx
+++ src/components/sections/About.jsx
@@ -24,7 +24,7 @@ export const About = () => {
         {/* CTA Buttons */}
         <div className="flex flex-wrap justify-center gap-4">
           <a
-            href="/CV_PFE_sirine_ELAYEB.pdf"
+            href="/CV_PFE_sirine_ELAYEB.pdf"  
             target="_blank"
             className="btn btn-primary"
           >
